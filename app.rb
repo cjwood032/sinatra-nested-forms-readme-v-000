7b9dd4859bcf484db3b88674b969dd3ce841a5cb
Christopher Wood <cjwood032@gmail.com>
2018-07-23 15:00:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative 'config/environment'
 require_relative 'models/student.rb'
 require_relative 'models/course.rb'
 class App < Sinatra::Base
+  get '/' do
+    erb :new
+  end
   post '/student' do
     @student = Student.new(params[:student])
     params[:student][:courses].each do |details|
